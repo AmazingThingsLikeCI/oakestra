6ae9b1cdcd31584e94e96775ece13c0ca61b8076
TheDarkPyotr <lucapint@live.it>
2024-05-17 09:56:03 +0200

    test bash linter

--- go_node_engine/build/build.sh
+++ go_node_engine/build/build.sh
@@ -1,5 +1,7 @@
-#arm build
+#!/bin/bash
+
+# arm build
 env GOOS=linux GOARCH=arm64 go build -o NodeEngine_arm-7 ../NodeEngine.go
 
-#amd build
+# amd build
 env GOOS=linux GOARCH=amd64 go build -o NodeEngine_amd64 ../NodeEngine.go
--- go_node_engine/build/install.sh
+++ go_node_engine/build/install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [ -z "$1" ]; then
     echo "Architecture not set"
     echo "Usage ./install.sh <architecture>"
